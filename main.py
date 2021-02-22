6135e922abe0ff11edd4b5e00a113584f9b7f430
aefe17 <aefe15@gmail.com>
2021-02-21 23:24:04 -0400

    added the actual bot commands, added the file for music func

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from Server.keep_alive import keep_alive
 from Functionalities.quotes import get_quote
 from Functionalities.challenge import get_challenge
+from discord.ext import commands
 
 #Loads token to be read by discord library 
 load_dotenv()
@@ -12,23 +13,25 @@ Token_Key = os.getenv("DISCORD_TOKEN")
 
 client = discord.Client()
 
+#Bot Commands 
+bot = commands.Bot(command_prefix = '$')
+@bot.command(name='Hola', help= 'Sends help and greets user')
+async def Hola(message):
+    await message.author.send('¡Hola soy el bot del colmadito! Estos son mis comandos: \n$Hola: Te saludo y te doy mi lista de comandos disponibles \n$Quote: Te doy una cita inspiracional\n$Reto: Te envio un coding challenge')
+@bot.command(name='Quote', help='Sends a inspirational Quote')
+async def Quote(message):
+    await message.send(f'Here is your quote: {get_quote()}')
+@bot.command(name='Challenge', help='Sends a coding challenge to the discord')
+async def Reto(message):
+    await message.send(f'Aqui esta el reto: {get_challenge()}')
+
+
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
-#Sends messages depending on the command sent through chat. 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return 
-    if message.content.startswith('$hola'): 
-        await message.author.send('¡Hola soy el bot del colmadito! Estos son mis comandos: \n$hola: Te saludo y te doy mi lista de comandos disponibles \n$quote: Te doy un quote \n$reto: Te envio un coding challenge')
-    if message.content.startswith('$quote'):
-        await message.channel.send(f'Here is your quote: {get_quote()}')
-    if message.content.startswith('$reto'):
-        await message.channel.send(f'Aqui esta el reto: {get_challenge()}')
-
 #runs server to keep alive the bot.
 keep_alive()
-#runs the bot with the provided token key.
+#runs the bot and its commands with the provided token key.
+bot.run(Token_Key)
 client.run(Token_Key)
