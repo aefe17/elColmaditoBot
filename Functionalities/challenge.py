331be0074eaf32c2a8c4c64e56c4b50dd86b0d3f
aefe17 <aefe15@gmail.com>
2021-02-22 23:13:34 -0400

    Added command and function for challengeList

--- Functionalities/challenge.py
+++ Functionalities/challenge.py
@@ -8,12 +8,23 @@ challenges = ['5865dd726b56998ec4000185', '555eded1ad94b00403000071', '517abf86d
               '550527b108b86f700000073f', '5324945e2ece5e1f32000370', '54a91a4883a7de5d7800009c']
 
 #this function gets you a challenge from the list of challenges and returns it as a string to be sent through chat.
+
 def get_challenge():
     response = requests.get(f"https://www.codewars.com/api/v1/code-challenges/{random.choice(challenges)}")
     json_data = json.loads(response.text)
     challenge = json_data['name']+ '\n' + 'Languages: ' +str(json_data['languages']) + '\n' + json_data['url']
     return challenge
 
+#this function adds a new challenge to the challenges list
 def new_challenge(challenge_url):
     challenge_id = challenge_url.split('/')[-3]
     challenges.append(challenge_id)
+
+def get_challengeList():
+    x = []
+    for i in range(len(challenges)):
+        response = requests.get(f"https://www.codewars.com/api/v1/code-challenges/{challenges[i]}")
+        json_data = json.loads(response.text)
+        challenge = json_data['url']
+        x.append(challenge)
+    return x
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from discord import channel
 from dotenv import load_dotenv
 from Server.keep_alive import keep_alive
 from Functionalities.quotes import get_quote
-from Functionalities.challenge import get_challenge, new_challenge
+from Functionalities.challenge import get_challenge, new_challenge, get_challengeList
 from discord.ext import commands
 
 #Loads token to be read by discord library
@@ -15,37 +15,36 @@ client = discord.Client()
 
 #Bot Commands 
 bot = commands.Bot(command_prefix = '$')
+
 @bot.command(name='Hola', help= 'Sends help and greets user')
 async def Hola(message):
     await message.author.send('¡Hola soy el bot del colmadito! Estos son mis comandos: \n$Hola: Te saludo y te doy mi lista de comandos disponibles \n$Quote: Te doy una cita inspiracional\n$Reto: Te envio un coding challenge')
+
 @bot.command(name='Quote', help='Sends a inspirational Quote')
 async def Quote(message):
     await message.send(f'Here is your quote: {get_quote()}')
-@bot.command(name='Challenge', help='Sends a coding challenge to the discord')
+
+@bot.command(name='challenge', help='Sends a coding challenge to the discord')
 async def Reto(message):
     await message.send(f'Aqui esta el reto: {get_challenge()}')
 
+@bot.command(name='challengeList', help='Returns Challenges list')
+async def challengeList(message):
+    # await message.channel.send(f'Esta es nuestra lista de retos acutal: {get_challengeList()}')
+    await message.send(f'Aqui nuestra lista de retos: {get_challengeList()}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return 
-    if message.content.startswith('$hola'): 
-        await message.author.send('¡Hola soy el bot del colmadito! Estos son mis comandos: \n$hola: Te saludo y te doy mi lista de comandos disponibles \n$quote: Te doy un quote \n$reto: Te envio un coding challenge')
-    if message.content.startswith('$quote'):
-        await message.channel.send(f'Here is your quote: {get_quote()}')
-    if message.content.startswith('$reto'):
-        print('Reto xD')
-        await message.channel.send(f'Aqui esta el reto: {get_challenge()}')
-    if message.content.startswith('$add_challenge'):
-        url = message.content.split(' ')[1]
-        new_challenge(url)
-        await message.channel.send('Added ' + url + ' to list!')
+@bot.command(name='addChallenge', help='Adds a new challenge into the challenges_list')
+async def addChallenge(message):
+    url = message.content.split(' ')[1]
+    new_challenge(url)
+    await message.channel.send('Added ' + url + ' to list!')
+
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
 #runs server to keep alive the bot.
 keep_alive()
+on_ready()
 #runs the bot and its commands with the provided token key.
 bot.run(Token_Key)
 client.run(Token_Key)
